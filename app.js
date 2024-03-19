4decaabf7c6a9e57bd73ed9a5e39645833e61467
eylulozatman <eylulozatman@gmail.com>
2024-03-19 22:24:49 +0300

    gitignore , idex.html  package changes

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const petRoutes = require('./petRoute');
-
+const path = require('path'); 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use('/pets', petRoutes);
 
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
